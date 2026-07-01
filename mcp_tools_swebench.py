db790c997eceebde41c82c903bd17bab40ad7d13
Kévin Kraft <kev.kraft13@gmail.com>
2026-07-01 13:21:24 +0200

    patchApply

--- mcp_tools_swebench.py
+++ mcp_tools_swebench.py
@@ -3,16 +3,18 @@ import subprocess
 import os
 
 mcp = FastMCP("swebench-tools")
-TESTBED = "testbed/"
+TESTBED = "/testbed"  # code patch
 
 # child (mcpswetoo) parent(agent-swe)
 def load_config() -> tuple[str, str]:
         task_image = os.getenv("SWE_DOCKER_IMG")  # from agent set environ()
         if not task_image:
             raise ValueError('Error missing image docker')
+
         eval_script = os.getenv("SWE_EVAL_SCRIPT")
         if not eval_script:
             raise ValueError('Error missing script to evaluate')
+
         print(task_image, eval_script)
         return task_image, eval_script
 
@@ -36,10 +38,20 @@ class McpToolSwe:
         self.id_containeur = run_docker.stdout.strip("\n")
         print(self.id_containeur)
 
-    def exec(self, cmd_exec: str, work_dir: str = TESTBED):
+    def exec(self, cmd_exec: str, work_dir: str = TESTBED) -> str:
         # for exec cmd tool
-        pass
+        if not self.id_containeur:
+            raise ValueError('Cannot find id_containeur')
+        exec_bash = ["docker", "exec", "-w", work_dir, self.id_containeur, "bash", "-c", cmd_exec]
+        containeur_exec = subprocess.run(executable=exec_bash, capture_output=True, text=True)
+        return containeur_exec
 
+    def clean_containeur(self) -> None:
+        if self.id_containeur:
+            clean = subprocess.run(["docker", "-rm", "-f", self.id_containeur])
 
 # read ... (exec avec docker chaques cmd dans un containeur)
 # @mcp.tool()
+
+
+# git -c core.fileMode=false dif
