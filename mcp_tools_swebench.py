13cffa3fa4b2cc4a6fe064592746d035e2f79c71
Kévin Kraft <kev.kraft13@gmail.com>
2026-07-01 11:56:41 +0200

    containeur_start

---
+++ mcp_tools_swebench.py
@@ -0,0 +1,42 @@
+from mcp.server.fastmcp import FastMCP
+import os
+import subprocess
+
+mcp = FastMCP("swebench-tools")
+
+def load_config() -> tuple[str, str]:
+        task_image = os.getenv("SWE_DOCKER_IMG")  # from agent set environ()
+        if not task_image:
+            raise ValueError('Error missing image docker')
+        eval_script = os.getenv("SWE_EVAL_SCRIPT")
+        if not eval_script:
+            raise ValueError('Error missing script to evaluate')
+        print(task_image, eval_script)
+        return task_image, eval_script
+
+
+class McpToolSwe:
+    def __init__(self, task_image, eval_script,id_containeur):
+        self.task_image = task_image
+        self.eval_script = eval_script
+        self.id_containeur: str | None = None  # remplir
+
+    def start_containeur(self, docker_id: str) -> None:
+        pull_image = subprocess.run(["docker", "pull", self.task_image],
+                              capture_output=True, text=True) # sotkc id_containeur
+
+        # aucun reseau d arriere plan
+        run_docker = subprocess.run(["docker", "run", "--network none", "-d", self.task_image, "sleep", "infinity"],
+                                    capture_output=True, # catch 
+                                    text=True) # print
+
+        self.id_containeur = run_docker.stdout.strip()
+        print(self.id_containeur)
+
+    def exec():
+        # for exec cmd tool
+        pass
+
+
+# read ...
+# @mcp.tool()
