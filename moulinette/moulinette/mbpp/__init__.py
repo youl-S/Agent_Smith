069eac80c9e7744276efab1244d60ce4556082eb
youl-S <youl.simonnet@gmail.com>
2026-07-02 10:20:46 +0200

    Delete moulinette directory

--- moulinette/moulinette/mbpp/__init__.py
+++
@@ -1,3 +0,0 @@
-# ABOUTME: MBPP subpackage for task loading and Docker-based code execution.
-# ABOUTME: Exports InteractMBPP and run_code_in_docker for direct use.
-from moulinette.mbpp.interact import InteractMBPP, run_code_in_docker
